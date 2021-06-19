1c43b901477ddb05d69f940c5a7d79207f1926f7
krushna2 <krushnadhawale2@gmail.com>
2021-06-19 10:35:24 +0530

    insurance policies added

--- src/components/HealthInsurance.js
+++ src/components/HealthInsurance.js
@@ -12,7 +12,7 @@ const HealthInsurance = () => {
                                     <Animated >
                                     <Col md={12} sm={12}>
                                         <div style={{boxShadow:" 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",marginTop:"5vh",paddingTop:"2vh",textAlign:"center"}}>
-                                            <h3>1) Aam Aadmi Bima Yojana</h3>
+                                            <h3> Aam Aadmi Bima Yojana</h3>
                                             The Aam Aadmi Bima Yojana is a security scheme that provides benefits to low income individuals in case of death or disability. The scheme is targeted at people working in 48 specific vocations like carpentry, handloom weaving, fishing, cobblers, auto drivers etc.<br>
                                             </br>
                                             For a premium of Rs. 300 per year, the scheme promises an assured sum of Rs. 30,000 after natural death, and Rs. 75,000 in case of accidental death of the policyholder. Only an earning member or head of the family can be insured under this scheme.
@@ -22,31 +22,49 @@ const HealthInsurance = () => {
                                             </div>
                                         </div>
                                         <div style={{boxShadow:" 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",marginTop:"5vh",paddingTop:"2vh",textAlign:"center"}}>
-                                            <h3>2) Ayushman Bharat Scheme</h3>
+                                            <h3> Ayushman Bharat Scheme</h3>
                                             The Ayushman Bharat Scheme is a health insurance scheme designed to unify the segmented health sector in India. It comprises two components:Health and Wellness Centres (HWC) and the Pradhan Mantri Jan Arogya Yojana (PM-JAY). The Health and Wellness Centres are supposed to be better developed versions of Primary Healthcare Centres
                                             <div>
                                                 <Button href="https://pmjay.gov.in/about/pmjay" type="submit" style={{height:"5vh",backgroundColor:"#a5c422",color:"white",marginTop:"2vh",marginBottom:"2vh"}} variant="contained">Read More</Button>
                                             </div>
                                         </div>
                                         <div style={{boxShadow:" 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",marginTop:"5vh",paddingTop:"2vh",textAlign:"center"}}>
-                                            <h3>3) Awaz Health Insurance Scheme</h3>
-                                            The Awaz Health Insurance Scheme was introduced in the state of Kerala and targeted at migrant workers. It offers labourers in Kerala a health insurance plan with an insured sum of Rs. 15,000. It also provides a death benefit of Rs. 2 Lakh in case of death of the policyholder.
-                                            <div>
-                                                <Button href="https://www.bankbazaarinsurance.com/health-insurance/awaz-health-insurance.html" type="submit" style={{height:"5vh",backgroundColor:"#a5c422",color:"white",marginTop:"2vh",marginBottom:"2vh"}} variant="contained">Read More</Button>
-                                            </div>
-                                        </div>
-                                        <div style={{boxShadow:" 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",marginTop:"5vh",paddingTop:"2vh",textAlign:"center"}}>
-                                            <h3>4) Central Government Health Scheme (CGHS)</h3>
+                                            <h3> Central Government Health Scheme (CGHS)</h3>
                                             The Central Government Health Scheme is a type of health plan provided by the Central Government of India. Only Central Government employees are eligible for this policy. These include Supreme Court judges, Central Railway Board employees, etc.
                                             <div>
                                                 <Button href="https://cghs.gov.in/ " type="submit" style={{height:"5vh",backgroundColor:"#a5c422",color:"white",marginTop:"2vh",marginBottom:"2vh"}} variant="contained">Read More</Button>
                                             </div>
                                         </div>
                                         <div style={{boxShadow:" 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",marginTop:"5vh",paddingTop:"2vh",textAlign:"center"}}>
-                                            <h3>5) Chief Minister's Comprehensive Insurance Scheme</h3>
+                                            <h3> Chief Minister's Comprehensive Insurance Scheme</h3>
                                             This state government scheme has been promoted by the government of Tamil Nadu for people residing in the state that are earning less than 75000 per annum. Medical expenses upto Rs. 5 lakh can be claimed using this scheme, and many private as well as government hospitals are a part of this scheme. It is basically a family floater plan by the government, in association with the United India Insurance Company.
                                             <div>
                                                 <Button href="https://madurai.nic.in/scheme/chief-ministers-comprehensive-health-insurance-scheme/" type="submit" style={{height:"5vh",backgroundColor:"#a5c422",color:"white",marginTop:"2vh",marginBottom:"2vh"}} variant="contained">Read More</Button>
+                                            </div>
+                                        </div>
+                                        <div style={{boxShadow:" 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",marginTop:"5vh",paddingTop:"2vh",textAlign:"center"}}>
+                                            <h3> Universal Health Insurance Scheme</h3>
+                                            This scheme was initiated by the Indian government to provide universal healthcare to persons below the poverty line and their families. It’s aim was to make universal healthcare accessible to these people.
+                                            The scheme covers hospitalization, accident as well as disability for eligible persons between 5 to 70 years of age. Both individual and group insurance policies can be availed using this scheme.
+                                            <div>
+                                                <Button href="https://uiic.co.in/taxonomy/term/1283" type="submit" style={{height:"5vh",backgroundColor:"#a5c422",color:"white",marginTop:"2vh",marginBottom:"2vh"}} variant="contained">Read More</Button>
+                                            </div>
+                                        </div>
+                                        <div style={{boxShadow:" 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",marginTop:"5vh",paddingTop:"2vh",textAlign:"center"}}>
+                                            <h3> Rashtriya Swasthya Bima Yojana</h3>
+                                            The Rashtriya Swasthya Bima Yojana was introduced by the Indian government to ensure that people working in the unorganized sector, like workers and labourers, had access to a good healthcare plan. Most of them don’t have any insurance and in case of hospitalization they have to pay the expenses out of their pocket.
+                                            To help them get good healthcare, the Ministry of Labour and Employment launched the scheme that covers labourers and their families.
+                                            <div>
+                                                <Button href="https://www.adityabirlacapital.com/abc-of-money/what-is-rsby-and-its-features" type="submit" style={{height:"5vh",backgroundColor:"#a5c422",color:"white",marginTop:"2vh",marginBottom:"2vh"}} variant="contained">Read More</Button>
+                                            </div>
+                                        </div>
+                                        <div style={{boxShadow:" 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",marginTop:"5vh",paddingTop:"2vh",textAlign:"center"}}>
+                                            <h3> Pradhan Mantri Suraksha Bima Yojana</h3>
+                                            This policy was launched in 2016, after it was evaluated that only 20% of the Indian population had insurance coverage for accidents. Anone between 18 to 70 years of age and having a bank account is eligible for this policy.
+
+                                            The assured sum offered in this scheme is Rs. 1 lakh for partial disability and Rs. 2 lakh for total disability and death due to accident. The premium for this scheme is Rs. 12 per year with the facility of auto-debit from the bank account.
+                                            <div>
+                                                <Button href="https://www.jansuraksha.gov.in/Forms-PMSBY.aspx" type="submit" style={{height:"5vh",backgroundColor:"#a5c422",color:"white",marginTop:"2vh",marginBottom:"2vh"}} variant="contained">Read More</Button>
                                             </div>
                                         </div>
                                     </Col>
